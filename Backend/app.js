b572b9ce0deb9ff12154ee33b0461defb1380598
Sudheer Pandit <sudheerpandit869@.com>
2025-03-26 01:22:42 +0530

    url change

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ const TaskApi = require("./route/task");
 
 app.use(cors(
   {
-    origin:["http://localhost:5173"],
+    origin:["https://task-management-navy-psi.vercel.app/"],
     methods:["POST", "GET"],
     credentials:true,
   }
